bb8bd715e0468ebd64729750b13a235b19898906
edcqaz33 <eddie8784@gmail.com>
2023-06-25 15:35:38 +0800

    refactor: modify mongoose config and one that text not comment

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
-const mongoose = require('mongoose') // 載入 mongoose
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
 // const Todo = require('./models/todo')
 const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
 
@@ -18,22 +18,10 @@ app.use(routes)
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 
 // 設定 port 3000
 app.listen(3000, () => {
