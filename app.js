dc6ca057b8e6cc3f0c44aebbf30bb00298d9090d
edcqaz33 <eddie8784@gmail.com>
2023-06-25 09:44:40 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose') // 載入 mongoose
 const app = express()
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const Todo = require('./models/todo')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -29,7 +30,7 @@ if (process.env.NODE_ENV !== 'production') {
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 // 設定首頁路由
 app.get('/', (req, res) => {
-  todo.find() // 取出 Todo model 裡的所有資料
+  Todo.find() // 取出 Todo model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
@@ -43,6 +44,14 @@ app.post('/todos', (req, res) => {
   const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
   return Todo.create({ name })     // 存入資料庫
     .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
+})
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('detail', { todo }))
     .catch(error => console.log(error))
 })
 
